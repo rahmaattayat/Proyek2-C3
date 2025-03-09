d5e939397e87e2bf08d82ec559913ad8884ef17d
pairus-portofolio <shevafairuz10@gmail.com>
2025-03-09 09:23:00 +0700

    update gerakinmusuh.c

--- src/musuh.c
+++ src/musuh.c
@@ -3,31 +3,38 @@
 #include <SDL3/SDL.h>
 #include <stdlib.h>
 
-void bikinMusuh(Musuh musuh[], int jumlah) {
-    for (int i = 0; i < JUMLAH; i++) {
+void bikinMusuh(Musuh musuh[], int jumlah)
+{
+    for (int i = 0; i < JUMLAH; i++)
+    {
         musuh[i].x = LEBAR_LAYAR;
         musuh[i].y = rand() % TINGGI_LAYAR;
         musuh[i].w = 50;
         musuh[i].h = 40;
         musuh[i].dx = -2.5f;
-        musuh[i].batasKiri = -50;       
+        musuh[i].batasKiri = -50;
         musuh[i].aktif = 1;
     }
 }
 
-void gerakinMusuh(Musuh* musuh) {
-    // Gerak horizontal (maju ke kiri)
-    musuh->x += musuh->dx;
+void gerakinMusuh(Musuh musuh[]) {
+    for (int i = 0; i < JUMLAH; i++) {
+        if (musuh[i].aktif) {  // Hanya gerakkan musuh yang aktif
+            musuh[i].x += musuh[i].dx;
 
-    // Jika musuh keluar dari layar, reset ke posisi awal dengan y random
-    if (musuh->x + musuh->w < musuh->batasKiri) {
-        musuh->x = LEBAR_LAYAR;
-        musuh->y = rand() % TINGGI_LAYAR;  // Muncul kembali dengan y acak
+            // Jika musuh keluar dari layar, reset posisinya
+            if (musuh[i].x + musuh[i].w < musuh[i].batasKiri) {
+                musuh[i].x = LEBAR_LAYAR;
+                musuh[i].y = rand() % TINGGI_LAYAR;
+            }
+        }
     }
 }
 
-void bikinGambarMusuh(SDL_Renderer* renderer, Musuh* musuh) {
-    SDL_SetRenderDrawColor(renderer, 255, 50, 50, 255);  
+
+void bikinGambarMusuh(SDL_Renderer *renderer, Musuh *musuh)
+{
+    SDL_SetRenderDrawColor(renderer, 255, 50, 50, 255);
     SDL_FRect kotakMusuh = {musuh->x, musuh->y, musuh->w, musuh->h};
     SDL_RenderFillRect(renderer, &kotakMusuh);
 }
